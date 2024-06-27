102f7b4496748c43cc92b96fa17673f89d51739d
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-06-26 23:32:09 -0500

    Actividad: Definiendo una función con parámetros predeterminados. Finalizada

--- profile/scripts/script.js
+++ profile/scripts/script.js
@@ -20,3 +20,24 @@ const contenidoPerfil = `
         <p>Hobby: ${misDatosFísicos.hobby || "No especificado"}</p>
 `
 perfilSelector.innerHTML = contenidoPerfil
+
+// Parámetro id tiene un valor predeterminado de "datos3"
+function printProfile (id = "datos3"){
+    // accede al contenedor <pre> con el id especificado
+    const datos = document.getElementById(id)
+    // template string con los datos del objeto
+    const contenido = `
+        <h1>${misDatosFísicos.nombre || "No especificado"}</h1>
+        <p>Edad: ${misDatosFísicos.edad || "No especificado"}</p>
+        <p>Dirección: ${misDatosFísicos.dirección.calle || "No especificado"}, ${misDatosFísicos.dirección.número || "No especificado"}, ${misDatosFísicos.dirección.ciudad || "No especificado"}.</p>
+        <p>Altura: ${misDatosFísicos.altura || "No especificado"}</p>
+        <p>Peso: ${misDatosFísicos.peso || "No especificado"}</p>
+        <p>Hobby: ${misDatosFísicos.hobby || "No especificado"}</p>
+    `
+     // Asigna el HTML generado a innerHTML del contenedor <pre>
+    datos.innerHTML = contenido
+}
+// Llama a la función printProfile sin argumentos
+// Imprimirá en "datos3" por defecto
+printProfile()
+// printProfile("datos2")
