9819457ff3585ff50135111368839c673bc0c508
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-06-26 21:05:06 -0500

    Actividad: Definiendo una plantilla. Finalizada

---
+++ profile/scripts/profile.js
@@ -0,0 +1,7 @@
+const nombre = "Jhonatan Rivera";
+const edad = 20;
+const profesión = "Administrador de Empresas";
+const país = "Perú";
+const departamento = "Piura"
+const provincia = "Sullana"
+document.write(`¡Hola Mi nombre es ${nombre}! Tengo ${edad} años de edad, soy de ${país}, ${departamento} - ${provincia}, y actualmente estudiante de la carrera de ${profesión}.`)
