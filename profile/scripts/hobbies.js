07c8ce3d67e0925bb919d13d5c02c0d2cd72554c
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-06-30 00:31:00 -0500

    Actividad: Creación de un nuevo objeto con un nuevo arreglo. Finalizada

--- profile/scripts/hobbies.js
+++ profile/scripts/hobbies.js
@@ -5,6 +5,8 @@ const hobbies = {
     conFamilia: ['cine', 'manualidades', 'jardinería', 'bicicleta', 'lectura']
 }
 console.log(hobbies);
+// Desestructura las listas de hobbies 
+const {enPareja, conAmigos, conFamilia} = hobbies
 // Extrae el tercer hobby de la lista "enPareja" y el cuarto de la lista "conFamilia".
 const {enPareja:[ , ,tercerHobbiesPareja], conFamilia: [ , , ,cuartoHobbiesFamilia]} = hobbies
 
@@ -12,3 +14,7 @@ const {enPareja:[ , ,tercerHobbiesPareja], conFamilia: [ , , ,cuartoHobbiesFamil
 document.getElementById('hobbies-seleccionados').innerHTML = `
     <p>Mi hobby favorito para hacer en pareja es <strong>${tercerHobbiesPareja}</strong>. Nos encanta mantenernos activos y saludables juntos. Para hacer en familia, mi hobby favorito es andar en <strong>${cuartoHobbiesFamilia}</strong>. Es una actividad divertida y nos permite disfrutar del aire libre y pasar tiempo de calidad juntos.</p>
 `
+
+// objeto con la propiedad hobbies, la cual contendrá un arreglo con todos los hobbies 
+const todos = {hobbies:[...enPareja, ...conAmigos, ...conFamilia]}
+console.log(todos);
