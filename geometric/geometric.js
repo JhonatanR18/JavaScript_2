c07072dfae9bee5ede2f784162c0a7647c898896
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-06-30 21:20:59 -0500

    Actividad: Cálculo del perímetro y área de figuras geométricas. Finalizada

---
+++ geometric/geometric.js
@@ -0,0 +1,41 @@
+// define tres objetos que representen rectángulos, cada uno con propiedades de altura y ancho.
+const rectángulos = {
+    Rectángulo_01: {ancho: 15, altura: 10},
+    Rectángulo_02: {ancho: 20, altura: 10},
+    Rectángulo_03: {ancho: 30, altura: 15},
+}
+// Desestructura los objetos rectángulo
+const {Rectángulo_01, Rectángulo_02, Rectángulo_03} = rectángulos
+
+// Desarrolla la función "area" para calcular los valores correspondientes de cada rectángulo.
+function functionArea (rectángulo){
+    return rectángulo.ancho * rectángulo.altura
+}
+
+// Desarrolla la función "perímetro" para calcular los valores correspondientes de cada rectángulo.
+function functionPerímetro(rectángulo){
+    return 2 * (rectángulo.ancho + rectángulo.altura)
+}
+
+// variable para acumular el contenido HTML
+let acumularAreas = "";
+// Itera sobre las propiedades del objeto rectángulos
+for(const key in rectángulos){
+    // Obtiene cada rectángulo del objeto "rectángulos"
+    const calcular = rectángulos[key];
+    // Calcula el área del rectángulo actual usando la función "functionArea"
+    const area = functionArea(calcular);
+    // Calcula el perímetro del rectángulo actual usando la función "functionPerímetro"
+    const perímetros = functionPerímetro(calcular);    
+    // Acumula el contenido HTML con la información del área de cada rectángulo.
+    acumularAreas = acumularAreas + `
+    <tr>
+        <td>${key}</td> 
+        <td>${area}</td>
+        <td>${perímetros}</td>
+    </tr>
+    `
+}
+/// Utilizamos `+=` para agregar dinámicamente las filas calculadas.
+// Esto permite añadir nuevas filas de datos sin reemplazar el contenido existente de la tabla.
+document.getElementById('area').innerHTML += acumularAreas
